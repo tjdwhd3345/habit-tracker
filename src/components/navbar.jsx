d3f8cda1d1026e63a51626edcd10cfd2f93962e1
sungjong <tjdwhd3345@gmail.com>
2020-11-08 22:33:51 +0900

    mocci / navbar 는 일반 div 보다 의미있는 nav 태그로 만드는 게 좋다고.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 class Navbar extends Component {
   render() {
     return (
-      <div className="navbar">
+      <nav className="navbar">
         <i className="fas fa-leaf navbar-logo"></i>
         <span>Habit Tracker</span>
-        <span className="navbar-count">{this.props.totalCounts}</span>
-      </div>
+        <span className="navbar-count">{this.props.totalCount}</span>
+      </nav>
     );
   }
 }
