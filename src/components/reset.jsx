a2d56bbdf2996fff7b14583302af276d52c520c2
sungjong <tjdwhd3345@gmail.com>
2020-11-08 21:26:33 +0900

    mocci / 카운트 초기화
    각 habit의 카운트만 초기화 한다.

--- src/components/reset.jsx
+++ src/components/reset.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class Reset extends Component {
+  handleReset = () => {
+    this.props.onReset();
+  };
   render() {
     return (
       <>
-        <button className="habits-reset">Reset All</button>
+        <button className="habits-reset" onClick={this.handleReset}>
+          Reset All
+        </button>
       </>
     );
   }
