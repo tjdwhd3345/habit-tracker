39244d0e2e77321f7dc5fce5aa6b606a61783510
sungjong <tjdwhd3345@gmail.com>
2020-11-08 21:27:51 +0900

    mocci / 모든 이벤트 app.jsx 로 이동

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -2,46 +2,22 @@ import React, { Component } from 'react';
 import Habit from './habit';
 
 class Habits extends Component {
-  state = {
-    habits: [
-      { id: 'habit1', name: 'Reading', count: 0 },
-      { id: 'habit2', name: 'Running', count: 0 },
-      { id: 'habit3', name: 'Coding', count: 0 },
-    ],
-  };
-
   handleIncrement = (habit) => {
-    // console.log('habits.jsx handleIncrement', habit);
-    const habits = [...this.state.habits];
-    const idx = habits.indexOf(habit);
-    habits[idx].count++;
-
-    this.setState({ habits: habits });
-    // this.setState({ habits }); // 위와 동일한 코드
+    this.props.onIncrement(habit);
   };
 
   handleDecrement = (habit) => {
-    // console.log('habits.jsx handleDecrement', habit);
-    const habits = [...this.state.habits];
-    const idx = habits.indexOf(habit);
-    // const count = habits[idx].count - 1;
-    // habits[idx].count = count < 0 ? 0 : count;
-    habits[idx].count =
-      (habits[idx].count--, habits[idx].count < 0 ? 0 : habits[idx].count);
-    this.setState({ habits: habits });
+    this.props.onDecrement(habit);
   };
 
   handleDelete = (habit) => {
-    const habits = this.state.habits.filter((item) => {
-      return item.id !== habit.id;
-    });
-    this.setState({ habits: habits });
+    this.props.onDelete(habit);
   };
 
   render() {
     return (
       <ul>
-        {this.state.habits.map((habit) => {
+        {this.props.habits.map((habit) => {
           return (
             <Habit
               key={habit.id}
