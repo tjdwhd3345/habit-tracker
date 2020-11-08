25e4683ff30fdd28532cf046c84953685391bba1
sungjong <tjdwhd3345@gmail.com>
2020-11-08 22:34:36 +0900

    mocci / [Ellie] Habits 에서 App 으로 전달
    Habbits 컴포넌트에 함수 적지 않고
    props 로 바로 전달해버림

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,34 +1,26 @@
 import React, { Component } from 'react';
 import Habit from './habit';
+import Addform from './addform';
 
 class Habits extends Component {
-  handleIncrement = (habit) => {
-    this.props.onIncrement(habit);
-  };
-
-  handleDecrement = (habit) => {
-    this.props.onDecrement(habit);
-  };
-
-  handleDelete = (habit) => {
-    this.props.onDelete(habit);
-  };
-
   render() {
     return (
-      <ul>
-        {this.props.habits.map((habit) => {
-          return (
-            <Habit
-              key={habit.id}
-              habit={habit}
-              onIncrement={this.handleIncrement}
-              onDecrement={this.handleDecrement}
-              onDelete={this.handleDelete}
-            />
-          );
-        })}
-      </ul>
+      <>
+        <Addform onAddHabit={this.props.onAddHabit}></Addform>
+        <ul>
+          {this.props.habits.map((habit) => {
+            return (
+              <Habit
+                key={habit.id}
+                habit={habit}
+                onIncrement={this.props.onIncrement}
+                onDecrement={this.props.onDecrement}
+                onDelete={this.props.onDelete}
+              />
+            );
+          })}
+        </ul>
+      </>
     );
   }
 }
