b46c1765bf74fc57313fa794caadabf4cf5153cc
sungjong <tjdwhd3345@gmail.com>
2020-11-08 20:58:48 +0900

    mocci / navbar, 추가입력 폼, 초기화 버튼 initialize

---
+++ src/components/addform.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Addform extends Component {
+  render() {
+    return (
+      <>
+        <input className="add-input" type="text" />
+        <button className="add-button">Add</button>
+      </>
+    );
+  }
+}
+
+export default Addform;
