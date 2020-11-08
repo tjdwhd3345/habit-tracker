7fb8bcd7f650e3f1b204c93eba0819c3400f79f3
sungjong <tjdwhd3345@gmail.com>
2020-11-08 19:12:13 +0900

    mocci / props 전달, state 변경
    각 habit object 의 state 는 habits.jsx 에 있으며
    habit.jsx 에 props 로 넘겨주고 있음.

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
-  state = {
-    count: 0,
+  handleIncrement = () => {
+    //   //console.log('increaseCount: ', event);
+    //   //이런식으로 직접 state 변수를 set 만 하면 리액트가 변화를 감지하지 못함.
+    //   //this.state.count += 1;
+    //   //setState 메소드를 사용하자
+    //   this.setState({ count: this.state.count + 1 });
+    this.props.onIncrement(this.props.habit);
   };
 
-  handleIncrement = (event) => {
-    //console.log('increaseCount: ', event);
-    //이런식으로 직접 state 변수를 set 만 하면 리액트가 변화를 감지하지 못함.
-    //this.state.count += 1;
-
-    //setState 메소드를 사용하자
-    this.setState({ count: this.state.count + 1 });
+  handleDecrement = () => {
+    //   const count = this.state.count - 1;
+    //   this.setState({ count: count < 0 ? 0 : count });
+    this.props.onDecrement(this.props.habit);
   };
 
-  handleDecrement = (event) => {
-    const count = this.state.count - 1;
-    this.setState({ count: count < 0 ? 0 : count });
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
   };
+
   render() {
-    console.log(this.props.habit);
+    // console.log('habit.jsx render: ', this.props.habit);
     const { name, count } = this.props.habit;
     return (
       <li className="habit">
@@ -37,7 +39,10 @@ class Habit extends Component {
         >
           <i className="fas fa-minus-square"></i>
         </button>
-        <button className="habit-button habit-delete">
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
           <i className="fas fa-trash"></i>
         </button>
       </li>
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -9,11 +9,48 @@ class Habits extends Component {
       { id: 'habit3', name: 'Coding', count: 0 },
     ],
   };
+
+  handleIncrement = (habit) => {
+    // console.log('habits.jsx handleIncrement', habit);
+    const habits = [...this.state.habits];
+    const idx = habits.indexOf(habit);
+    habits[idx].count++;
+
+    this.setState({ habits: habits });
+    // this.setState({ habits }); // 위와 동일한 코드
+  };
+
+  handleDecrement = (habit) => {
+    // console.log('habits.jsx handleDecrement', habit);
+    const habits = [...this.state.habits];
+    const idx = habits.indexOf(habit);
+    // const count = habits[idx].count - 1;
+    // habits[idx].count = count < 0 ? 0 : count;
+    habits[idx].count =
+      (habits[idx].count--, habits[idx].count < 0 ? 0 : habits[idx].count);
+    this.setState({ habits: habits });
+  };
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => {
+      return item.id !== habit.id;
+    });
+    this.setState({ habits: habits });
+  };
+
   render() {
     return (
       <ul>
         {this.state.habits.map((habit) => {
-          return <Habit key={habit.id} habit={habit} />;
+          return (
+            <Habit
+              key={habit.id}
+              habit={habit}
+              onIncrement={this.handleIncrement}
+              onDecrement={this.handleDecrement}
+              onDelete={this.handleDelete}
+            />
+          );
         })}
       </ul>
     );
