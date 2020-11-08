1ae8bbad4e7a4adcd4fe5a9dba031e8f090119a1
sungjong <tjdwhd3345@gmail.com>
2020-11-08 20:59:17 +0900

    mocci / navbar, addform, habits, reset 영역 추가

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app.css';
+import Navbar from './components/navbar';
+import Addform from './components/addform';
 import Habits from './components/habits';
+import Reset from './components/reset';
 
-function App() {
-  return <Habits></Habits>;
+//function App() {
+class App extends Component {
+  state = {
+    habits: [
+      { id: 'habit1', name: 'Reading', count: 0 },
+      { id: 'habit2', name: 'Running', count: 0 },
+      { id: 'habit3', name: 'Coding', count: 0 },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    // console.log('habits.jsx handleIncrement', habit);
+    const habits = [...this.state.habits];
+    const idx = habits.indexOf(habit);
+    habits[idx].count++;
+
+    this.setState({ habits: habits });
+    // this.setState({ habits }); // 위와 동일한 코드
+  };
+
+  handleDecrement = (habit) => {
+    // console.log('habits.jsx handleDecrement', habit);
+    const habits = [...this.state.habits];
+    const idx = habits.indexOf(habit);
+    // const count = habits[idx].count - 1;
+    // habits[idx].count = count < 0 ? 0 : count;
+    habits[idx].count =
+      (habits[idx].count--, habits[idx].count < 0 ? 0 : habits[idx].count);
+    this.setState({ habits: habits });
+  };
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => {
+      return item.id !== habit.id;
+    });
+    this.setState({ habits: habits });
+  };
+
+  render() {
+    return (
+      <>
+        <Navbar></Navbar>
+        <Addform></Addform>
+        <Habits
+          habits={this.state.habits}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+        ></Habits>
+        <Reset></Reset>
+      </>
+    );
+  }
 }
 
 export default App;
